d8f2122018800f522ceddd940adecc4a8b573fa1
Tov Are Jacobsen <mail@tovare.com>
2021-01-07 05:45:57 +0100

    experiment

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
   },
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        '8': 'repeat(8, minmax(0, 1fr))',
+        '12': 'repeat(12, minmax(0, 1fr))',
+        '31': 'repeat(31, minmax(0, 1fr))',
+      }
+    }
   },
   variants: {
     extend: {},
